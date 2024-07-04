1540d55806bcd37134798865e32c81e6bbf040bc
Cory Bell <corybell21@gmail.com>
2024-07-04 11:44:58 -0500

    update intro

--- src/content/intro.ts
+++ src/content/intro.ts
@@ -1,8 +1,12 @@
 export const INTRO_TITLE = `Hi, I'm Cory`
 
 export const INTRO_BODY = `
-  I'm a full stack developer located in St. Paul, MN. My career began in
-  2012 and includes a range of large corporate clients to small
-  businesses. I leverage battle-tested design patterns as well as current
-  industry trends to deliver high-quality solutions. I'm currently
-  interested in the JAMstack and GraphQL.`
+  I'm an independent software contractor and the principal at Bell Code. 
+  
+  My career began in 2012 and includes a range of large corporate clients to small
+  businesses. 
+  
+  I leverage battle-tested design patterns as well as current
+  industry trends to deliver high-quality solutions. 
+  
+  I'm available for contract or full-time work.`
