d41bb18b69d3f67c398ed39ee138661fa8ebf127
Cory Bell <corybell21@gmail.com>
2024-07-04 11:15:51 -0500

    add photos, style intro better

--- src/components/intro/index.tsx
+++ src/components/intro/index.tsx
@@ -7,12 +7,12 @@ export function Intro() {
       <h1 className='text-4xl text-center mb-6 md:mb-12 lg:mb-12'>
         {INTRO_TITLE}
       </h1>
-      <p className='text-sm md:text-base lg:text-base text-center mb-12 md:mb-24 lg:mb-24'>
+      <p className='text-sm md:text-base lg:text-base text-center max-w-md mx-auto mb-12 md:mb-24 lg:mb-24'>
         {INTRO_BODY}
       </p>
-      <div className='relative h-96 w-full mb-12 md:mb-24 lg:mb-24'>
+      <div className='relative h-48 w-full mb-12 md:mb-24 lg:mb-24'>
         <Image
-          src='/Cory.jpg'
+          src='/Cory-Red-Shirt-2.jpg'
           alt='Bell Code Logo'
           className='object-contain'
           fill
