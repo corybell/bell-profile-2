3313abb9845ec2545fed9acf47fa61c272a383c0
Cory Bell <cory.bell.developer@gmail.com>
2025-02-19 20:10:09 -0600

    buttons

--- src/components/intro/index.tsx
+++ src/components/intro/index.tsx
@@ -1,10 +1,15 @@
-import { INTRO_BODY, INTRO_TITLE } from '@/content/intro'
+import {
+  GITHUB_URL,
+  INTRO_BODY,
+  INTRO_TITLE,
+  LINKEDIN_URL,
+} from '@/content/intro'
 import Image from 'next/image'
 import Link from 'next/link'
 
 export function Intro() {
   return (
-    <div className='flex flex-col gap-8'>
+    <div className='flex flex-col gap-8 mb-12'>
       <div className='relative mt-16 h-48 w-full'>
         <Image
           src='/profile.png'
@@ -14,7 +19,9 @@ export function Intro() {
           priority
         />
       </div>
-      <h1 className='text-center text-3xl font-semibold uppercase'>{INTRO_TITLE}</h1>
+      <h1 className='text-center text-3xl font-semibold uppercase'>
+        {INTRO_TITLE}
+      </h1>
       <p className='mx-auto max-w-md text-center text-sm md:text-base lg:text-base'>
         {INTRO_BODY}
       </p>
@@ -24,15 +31,40 @@ export function Intro() {
           href={'/resume_V4.pdf'}
           target='_blank'
           rel='noopener noreferrer'
-          className='bg-navy-blue border-navy-blue rounded-lg border-4 border-solid px-12 py-2 text-white'
+          style={{ background: `url('/buttons/PrimaryLight.svg') no-repeat` }}
+          className='px-12 py-2 text-white'
         >
           Download Resume
         </Link>
         <Link
           href='/recruiters'
-          className='border-navy-blue rounded-lg border-4 border-solid px-12 py-2'
+          style={{
+            background: `url('/buttons/SecondaryLight.svg') no-repeat`,
+            height: '44px',
+            width: '216px',
+            lineHeight: '30px'
+          }}
+          className='px-12 py-2'
         >
           Recruiter Guide
+        </Link>
+      </div>
+      <div className='flex justify-center gap-6'>
+        <Link href={GITHUB_URL} target='_blank'>
+          <Image
+            src='/icons/icon-github.svg'
+            alt='github'
+            width={40}
+            height={40}
+          />
+        </Link>
+        <Link href={LINKEDIN_URL} target='_blank'>
+          <Image
+            src='/icons/icon-linkedin.png'
+            alt='linkedin'
+            width={40}
+            height={40}
+          />
         </Link>
       </div>
     </div>
--- src/content/intro.ts
+++ src/content/intro.ts
@@ -4,3 +4,7 @@ export const INTRO_BODY = `
   Hi, I'm Cory. A results-driven full-stack developer blending
 extensive expertise, battle-tested designs, and market trends to optimize
 software platforms for diverse clients across industries.`
+
+export const GITHUB_URL = 'github.com/corybell'
+
+export const LINKEDIN_URL = 'www.linkedin.com/in/corybell'
