87c163dc225af7e63c1d4f039a560cb9a0bc987e
Cory Bell <cory.bell.developer@gmail.com>
2025-02-21 13:18:21 -0600

    priority on shape img

--- src/components/ProjectShowcase/ProjectTile.tsx
+++ src/components/ProjectShowcase/ProjectTile.tsx
@@ -12,6 +12,7 @@ export function ProjectTile({ projectTile }: { projectTile: ProjectTileType }) {
         src={`/shapes/${projectTile.shape}`}
         alt={projectTile.title}
         fill
+        priority
       />
       <div className='absolute -top-3 flex flex-col items-center gap-4 px-8'>
         <div className='relative h-48 w-full'>
